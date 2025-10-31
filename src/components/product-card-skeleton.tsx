a36d0500c6eb6e0e4745a1588ce3c24d2571c220
Hugo Ezequiel Medina Carabajal <huguitoezequiel8@gmail.com>
2025-10-31 12:28:49 +0000

    puedes hacer que sea mas parecida a la estructura de los productos?

--- src/components/product-card-skeleton.tsx
+++ src/components/product-card-skeleton.tsx
@@ -2,13 +2,34 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export default function ProductCardSkeleton() {
   return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[250px] w-full rounded-lg" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-3/4" />
-        <Skeleton className="h-4 w-1/2" />
-        <Skeleton className="h-8 w-full mt-4" />
-        <Skeleton className="h-8 w-full" />
+    <div className="w-full overflow-hidden rounded-lg border bg-card shadow-sm flex flex-col">
+      {/* Image Skeleton */}
+      <Skeleton className="aspect-square w-full" />
+      
+      <div className="flex flex-1 flex-col p-4">
+        <div className="flex-1">
+          {/* Brand Skeleton */}
+          <Skeleton className="h-5 w-1/4" />
+          {/* Model Skeleton */}
+          <Skeleton className="h-7 w-3/4 mt-2" />
+
+          <div className="mt-4 space-y-2">
+            {/* Price Skeleton */}
+            <Skeleton className="h-8 w-1/2" />
+            {/* Installments Skeleton */}
+            <Skeleton className="h-5 w-full" />
+          </div>
+        </div>
+        
+        <div className="mt-6">
+            <div className="grid grid-cols-2 gap-2">
+                {/* Button Skeletons */}
+                <Skeleton className="h-10 w-full" />
+                <Skeleton className="h-10 w-full" />
+            </div>
+            {/* Main Button Skeleton */}
+            <Skeleton className="h-10 w-full mt-4" />
+        </div>
       </div>
     </div>
   )
