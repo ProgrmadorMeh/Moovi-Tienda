685acb540b42d57928ae8d8b1459f538ef16eae2
FEDE NUÑEZ <nfede41@gmail.com>
2025-10-31 22:13:35 +0000

    Implementa un esqueleto de carga a toda la aplicacion a cada parte, e im

---
+++ src/app/loading.tsx
@@ -0,0 +1,32 @@
+
+import ProductCardSkeleton from "@/components/product-card-skeleton";
+import FilterSkeleton from "@/components/filter-skeleton";
+import { Skeleton } from "@/components/ui/skeleton";
+import HeroSection from "@/components/hero-section";
+
+export default function Loading() {
+  return (
+    <>
+      <HeroSection />
+      <div id="product-catalog" className="container mx-auto px-4 py-12">
+        <div className="py-12 md:py-16">
+          <Skeleton className="mb-8 grid h-[46px] w-full grid-cols-2 rounded-md p-1 md:grid-cols-4" />
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-4">
+            <aside className="lg:col-span-1">
+              <div className="sticky top-20">
+                <FilterSkeleton />
+              </div>
+            </aside>
+            <main className="lg:col-span-3">
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
+                {Array.from({ length: 8 }).map((_, i) => (
+                  <ProductCardSkeleton key={i} />
+                ))}
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
