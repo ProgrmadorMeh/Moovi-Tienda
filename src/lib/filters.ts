e3c4b9c065b0f0620e5c60f56644a2e133face52
FEDE NUÑEZ <nfede41@gmail.com>
2025-11-06 22:18:55 +0000

    fiilters.ts tiene variables inutilizadas, como procesador y sistema oper

--- src/lib/filters.ts
+++ src/lib/filters.ts
@@ -1,34 +1,11 @@
-
 // src/lib/filters.ts
 
 interface PredefinedFilters {
-  Procesador: string[];
   RAM: string[];
   Almacenamiento: string[];
-  'Sistema Operativo': string[];
 }
 
 export const predefinedFilters: PredefinedFilters = {
-  Procesador: [
-    "Apple A15 Bionic",
-    "Apple A16 Bionic",
-    "Apple A18",
-    "Apple A18 Pro",
-    "Snapdragon 7s Gen 2",
-    "Snapdragon 6 Gen 1",
-    "Snapdragon 6 Gen 3",
-    "Snapdragon 6s Gen 3",
-    "Snapdragon 8 Elite",
-    "Exynos 1280",
-    "Exynos 1580",
-    "Helio G99",
-    "Helio G85",
-    "Dimensity 7300",
-    "Dimensity 7400",
-    "Dimensity 8350",
-    "Exynos 990",
-    "Snapdragon 865",
-  ],
   RAM: [
     "4 GB",
     "6 GB",
@@ -42,13 +19,5 @@ export const predefinedFilters: PredefinedFilters = {
     "256 GB",
     "512 GB",
     "1 TB",
-  ],
-  'Sistema Operativo': [
-    "iOS 15",
-    "iOS 16",
-    "iOS 18",
-    "Android 14",
-    "Android 15",
-    "Android 10",
   ],
 };
